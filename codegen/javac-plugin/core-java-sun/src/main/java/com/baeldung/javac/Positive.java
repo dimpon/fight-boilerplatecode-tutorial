95b92896f87d5e2dca2a2d50d1f1c7620cfb1b4a
Dmitry Ponomarev <dimpon@gmail.com>
2018-09-05 23:03:35 +0200

    remove codegen

--- codegen/javac-plugin/core-java-sun/src/main/java/com/baeldung/javac/Positive.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.javac;
-
-import java.lang.annotation.*;
-
-@Documented
-@Retention(RetentionPolicy.CLASS)
-@Target({ElementType.PARAMETER, ElementType.TYPE, ElementType.LOCAL_VARIABLE})
-public @interface Positive {
-}
