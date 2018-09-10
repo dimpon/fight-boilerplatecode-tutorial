2dc3950d134fa5831c1b8840a3de791de79018da
Dmitry Ponomarev <dimpo@gmail.com>
2018-09-10 11:20:58 +0200

    lazy init

--- src/main/java/com/dimpon/tutorals/lombok/delegation/Start.java
+++ src/main/java/com/dimpon/tutorals/lombok/delegation/Start.java
@@ -1,32 +1,41 @@
 package com.dimpon.tutorals.lombok.delegation;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 
 /**
  * @author Dmitrii Ponomarev
  */
-@Slf4j
+//@Slf4j
 public class Start {
+
+	private static final LogDelegator LOG = LogDelegator.of((() ->
+			LoggerFactory.getLogger(Start.class))
+	);
+
+
 
 	public static void main(String[] args) {
 
 		SimpleDelegate delegate = SimpleDelegate.of(new ArrayList<>());
-		log.info("size = " + delegate.size());
-		delegate.forEach(o -> {});
+        LOG.info("size = " + delegate.size());
+		delegate.forEach(o -> {
+		});
 
 		Octopus o = new Octopus();
 		Fisher f = new Fisher();
 
-		OctopusDelegate delegate1 = OctopusDelegate.of(o,f);
+		OctopusDelegate delegate1 = OctopusDelegate.of(o, f);
 
 		delegate1.head();
 		delegate1.leg1();
 		delegate1.catchIt(o);
-		log.info(delegate1.iSeeOctopus());
+		LOG.info(delegate1.iSeeOctopus());
 
-		//delegate1.sayWhoIam();   //lombok is not able to generate method, it is ambiguous
+		// delegate1.sayWhoIam(); //lombok is not able to generate method, it is ambiguous
 
 	}
 }
